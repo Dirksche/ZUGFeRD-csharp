9f4ce6ffb8d4be7514f374cd8b13eb58c854d3b0
Dirksche <mozilla@Dirk-Busse.de>
2025-09-09 22:52:01 +0300

    Added tests to generate e-invoices with a discount on the whole invoice.

---
+++ ZUGFeRD.Test/CreateAndCompareAgainstReferenceFile/DscountOverWholeInvoiceTest.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace s2industries.ZUGFeRD.Test.CreateAndCompareAgainstReferenceFile
+{
+    [TestClass]
+    public class DscountOverWholeInvoiceTest : TestBase
+    {
+        private InvoiceProviders.DscountOverWholeInvoice _InvoiceProvider = new InvoiceProviders.DscountOverWholeInvoice();
+
+        [DataTestMethod]
+
+        [DataRow(ZUGFeRDVersion.Version20, Profile.Comfort, ZUGFeRDFormats.CII)]
+        [DataRow(ZUGFeRDVersion.Version20, Profile.Extended, ZUGFeRDFormats.CII)]
+
+        [DataRow(ZUGFeRDVersion.Version23, Profile.Comfort, ZUGFeRDFormats.CII)]
+        [DataRow(ZUGFeRDVersion.Version23, Profile.Extended, ZUGFeRDFormats.CII)]
+        [DataRow(ZUGFeRDVersion.Version23, Profile.XRechnung, ZUGFeRDFormats.CII)]
+
+        // I'm not testing UBL as much as CII, because I believe that most people will be using CII.
+        // UBL is not supported in ZUGFeRD-PDF files. Therefore, if you plan to embed your e-invoice
+        // in a PDF file to make a ZUGFeRD file, you will have to use CII.
+        // Furthermore, UBL has other limitations. E.g. it doesn't support credit notes. You will
+        // need to use negative invoices instead, when using UBL.
+        [DataRow(ZUGFeRDVersion.Version23, Profile.XRechnung, ZUGFeRDFormats.UBL)]
+
+        public void CreateAndCompare(ZUGFeRDVersion version, Profile profile, ZUGFeRDFormats format)
+        {
+            var desc = _InvoiceProvider.CreateInvoice();
+            string className = GetType().Name;
+            string filename = $"{className}_{version}_{profile}_{format}.xml";
+
+            string fullFilePathOfGeneratedInvoice = _makeSurePathIsCrossPlatformCompatible(System.IO.Path.GetTempPath() + filename);
+            using (var fs = new FileStream(fullFilePathOfGeneratedInvoice, FileMode.Create))
+            {
+                desc.Save(fs, version, profile, format);
+            }
+
+            string fullFilePathOfReferenceInvoice = _makeSurePathIsCrossPlatformCompatible(@"..\\..\\..\\ReferenceFiles\\" + filename);
+
+            string expected = File.ReadAllText(fullFilePathOfReferenceInvoice);
+            string actual = File.ReadAllText(fullFilePathOfGeneratedInvoice);
+
+            // If the generated file does not match the reference file, please check whether the changes are valid.
+            // - Compare the generated file with the reference file (e.g., using BeyondCompare) and analyse the differences.
+            // - Check the validity of the generated file (e.g. with https://www.portinvoice.com/).
+            // - If the changes are valid, replace the reference file with the generated file.
+            // The following validation warnings are acceptable:
+            //   [VD-Valitool-71]-Es konnten keine Angaben wie z.B. Geschäftsführer, Handelsregistereintrag oder ähnliche Angaben
+            //                    gefunden werden. (SubjectCode=REG). Bitte prüfen Sie, ob Sie diese Daten ggf. im Freitext angegeben
+            //                    haben oder diese Angaben für Ihre Rechnungsstellung nicht relevant sind.
+            //      --> This is acceptable because the creator of the e-invoice can fix this themselves by specifying the #-syntax (#REG#) in the text.
+            //   [VD-Valitool-148]-Die Rechnung liegt im Format XRechnung vor. Sie enthält Skontobedingungen. Die XRechnung verlangt
+            //                     die Angabe von Skontobedingungen in strukturierter Form. Die Skontobedingungen liegen jedoch
+            //                     ausschließlich als Freitext vor.
+            //      --> This is acceptable because the creator of the e-invoice can fix this themselves by specifying the #-syntax (#SKONTO#...) in the text.
+            //   [VD-Valitool-160]-Die Umsatzsteuer-Identifikationsnummer entspricht nicht dem national vorgegebenen Format: Prüfsumme oder Prüfziffer ist falsch.
+            //      --> This is acceptable for this test, as the creator of a correct e-invoice should provide a valid VAT ID.
+            AssertEqualSuppressDetails(expected, actual, $"The generated e-invoice '{fullFilePathOfGeneratedInvoice}' does not match the reference file." + Environment.NewLine +
+                $"Please validate the changes and update the reference files.");
+        }
+    }
+}
---
+++ ZUGFeRD.Test/InvoiceProviders/DscountOverWholeInvoice.cs
@@ -0,0 +1,210 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace s2industries.ZUGFeRD.Test.InvoiceProviders
+{
+    /// <summary>
+    /// Class for creating an example invoice with a discount over the whole invoice.
+    /// </summary>
+    internal class DscountOverWholeInvoice
+    {
+        /// <summary>
+        /// Creates an example invoice with a discount over the whole invoice.
+        /// </summary>
+        /// <returns>InvoiceDescriptor which contains the generated invoice</returns>
+        internal InvoiceDescriptor CreateInvoice()
+        {
+            InvoiceDescriptor desc = InvoiceDescriptor.CreateInvoice("471102", new DateTime(2024, 03, 05), CurrencyCodes.EUR);
+
+            // The BusinessProcess is required, when creating E-Invoices with the XRechnung profile in CII format.
+            desc.BusinessProcess = "urn:fdc:peppol.eu:2017:poacc:billing:01:1.0";
+
+            desc.Name = "WARENRECHNUNG";
+            desc.ActualDeliveryDate = new DateTime(2024, 03, 05);
+
+            desc.ReferenceOrderNo = "04011000-12345-34";
+
+            desc.SetSeller(name: "Lieferant GmbH",
+                           postcode: "80333",
+                           city: "München",
+                           street: "Lieferantenstraße 20",
+                           country: CountryCodes.DE,
+                           id: "Kennung des Verkäufers"
+                           );
+            desc.SetSellerElectronicAddress("DE123456789", ElectronicAddressSchemeIdentifiers.GermanyVatNumber);
+            desc.SetSellerContact(name: "Max Mustermann",
+                                  orgunit: "Muster-Einkauf",
+                                  emailAddress: "Max@Mustermann.de",
+                                  phoneno: "+49891234567",
+                                  faxno: "+49891234568"
+                                 );
+            desc.AddSellerTaxRegistration("DE123456789", TaxRegistrationSchemeID.VA);
+
+            desc.SetBuyer(name: "Kunden AG Mitte",
+                          postcode: "69876",
+                          city: "Frankfurt",
+                          street: "Kundenstraße 15",
+                          country: CountryCodes.DE,
+                          id: "GE2020211"
+                          );
+            desc.SetBuyerElectronicAddress("DE123123123", ElectronicAddressSchemeIdentifiers.GermanyVatNumber);
+
+            desc.AddNote("Rechnung gemäß Bestellung Nr. 2018-471331 vom 01.03.2018.");
+            desc.AddNote("Es bestehen Rabatt- und Bonusvereinbarungen.", SubjectCodes.AAK);
+            desc.AddNote("Lieferant GmbH\r\nLieferantenstraße 20\r\n80333 München\r\nDeutschland\r\nGeschäftsführer: Hans Muster\r\nHandelsregisternummer: H A 123\r\n",
+                         SubjectCodes.REG);
+
+            // add three articles
+            {
+                TradeLineItem tradeLineItem = desc.AddTradeLineItem("BERLTHYROX 100UG",     // BT-153 Artikelname
+                                                                    14.0439m,               // BT-146 der Preis eines Artikels ohne Umsatzsteuer nach Abzug des Nachlass auf den Artikelpreis
+                                                                    QuantityCodes.XPP,      // BT-130 Code der Maßeinheit der in Rechnung gestellten Menge
+                                                                    "My description",       // BT-154 Artikelbeschreibung
+                                                                    null,                   // BT-149 die Anzahl von Artikeleinheiten, für die der Preis gilt
+                                                                    null,                   // BT-148 der Einheitspreis **ohne Umsatzsteuer** vor Abzug des Nachlass auf den Artikelpreis
+                                                                    3,                      // BT-129 in Rechnung gestellte Menge
+                                                                    null,                   // BT-131 Nettobetrag der Rechnungsposition
+                                                                    TaxTypes.VAT,           // BT-151-0
+                                                                    TaxCategoryCodes.S,     // BT-151 Code der Umsatzsteuerkategorie des in Rechnung gestellten Artikels
+                                                                    19,                     // BT-152 Umsatzsteuersatz für den in Rechnung gestellten Artikel
+                                                                    "My comment",           // BT-127 Freitext zur Rechnungsposition
+                                                                    null,                   // BT-157 Kennung eines Artikels nach registriertem Schema
+                                                                    "04169807",             // BT-155 eine dem Artikel vom Verkäufer zugewiesene Kennung
+                                                                    null,                   // BT-156 eine dem Artikel vom Käufer zugewiesene Kennung
+                                                                    null, // DeliveryNoteID		// BT-X-92
+                                                                    null, // DeliveryNoteDate	// BT-X-94
+                                                                    null, // BuyerOrderLineID	// BT-132 Referenz zur Bestellposition
+                                                                    null, // BuyerOrderID		// BT-132 Referenz zur Bestellposition
+                                                                    null, // Bestelldatum		// BT-132 Referenz zur Bestellposition
+                                                                    null, // Start of billing period
+                                                                    null); // End of billing period
+
+                tradeLineItem.AddApplicableProductCharacteristic("Package size", "100 St");
+                tradeLineItem.AddApplicableProductCharacteristic("Dosage form", "Tablets");
+            }
+            {
+                TradeLineItem tradeLineItem = desc.AddTradeLineItem("INFECTOTRIMET 100MG",  // BT-153 Artikelname
+                                                                    20.7357m,               // BT-146 der Preis eines Artikels ohne Umsatzsteuer nach Abzug des Nachlass auf den Artikelpreis
+                                                                    QuantityCodes.XPP,      // BT-130 Code der Maßeinheit der in Rechnung gestellten Menge
+                                                                    "My 2. description",    // BT-154 Artikelbeschreibung
+                                                                    null,                   // BT-149 die Anzahl von Artikeleinheiten, für die der Preis gilt
+                                                                    null,                   // BT-148 der Einheitspreis **ohne Umsatzsteuer** vor Abzug des Nachlass auf den Artikelpreis
+                                                                    10,                     // BT-129 in Rechnung gestellte Menge
+                                                                    null,                   // BT-131 Nettobetrag der Rechnungsposition
+                                                                    TaxTypes.VAT,           // BT-151-0
+                                                                    TaxCategoryCodes.S,     // BT-151 Code der Umsatzsteuerkategorie des in Rechnung gestellten Artikels
+                                                                    7,                      // BT-152 Umsatzsteuersatz für den in Rechnung gestellten Artikel
+                                                                    "My 2. comment",        // BT-127 Freitext zur Rechnungsposition
+                                                                    null,                   // BT-157 Kennung eines Artikels nach registriertem Schema
+                                                                    "02736107",             // BT-155 eine dem Artikel vom Verkäufer zugewiesene Kennung
+                                                                    null,                   // BT-156 eine dem Artikel vom Käufer zugewiesene Kennung
+                                                                    null, // DeliveryNoteID		// BT-X-92
+                                                                    null, // DeliveryNoteDate	// BT-X-94
+                                                                    null, // BuyerOrderLineID	// BT-132 Referenz zur Bestellposition
+                                                                    null, // BuyerOrderID		// BT-132 Referenz zur Bestellposition
+                                                                    null, // Bestelldatum		// BT-132 Referenz zur Bestellposition
+                                                                    null, // Start of billing period
+                                                                    null); // End of billing period
+
+                tradeLineItem.AddApplicableProductCharacteristic("Package size", "50 St");
+                tradeLineItem.AddApplicableProductCharacteristic("Dosage form", "Tablets");
+            }
+            {
+                TradeLineItem tradeLineItem = desc.AddTradeLineItem("METOHEXAL-SUCC 23.75MG",   // BT-153 Artikelname
+                                                                    12.6666m,               // BT-146 der Preis eines Artikels ohne Umsatzsteuer nach Abzug des Nachlass auf den Artikelpreis
+                                                                    QuantityCodes.XPP,      // BT-130 Code der Maßeinheit der in Rechnung gestellten Menge
+                                                                    "My 3. description",    // BT-154 Artikelbeschreibung
+                                                                    null,                   // BT-149 die Anzahl von Artikeleinheiten, für die der Preis gilt
+                                                                    null,                   // BT-148 der Einheitspreis **ohne Umsatzsteuer** vor Abzug des Nachlass auf den Artikelpreis
+                                                                    2,                      // BT-129 in Rechnung gestellte Menge
+                                                                    null,                   // BT-131 Nettobetrag der Rechnungsposition
+                                                                    TaxTypes.VAT,           // BT-151-0
+                                                                    TaxCategoryCodes.S,     // BT-151 Code der Umsatzsteuerkategorie des in Rechnung gestellten Artikels
+                                                                    19,                     // BT-152 Umsatzsteuersatz für den in Rechnung gestellten Artikel
+                                                                    "My 3. comment",        // BT-127 Freitext zur Rechnungsposition
+                                                                    null,                   // BT-157 Kennung eines Artikels nach registriertem Schema
+                                                                    "00850419",             // BT-155 eine dem Artikel vom Verkäufer zugewiesene Kennung
+                                                                    null,                   // BT-156 eine dem Artikel vom Käufer zugewiesene Kennung
+                                                                    null, // DeliveryNoteID		// BT-X-92
+                                                                    null, // DeliveryNoteDate	// BT-X-94
+                                                                    null, // BuyerOrderLineID	// BT-132 Referenz zur Bestellposition
+                                                                    null, // BuyerOrderID		// BT-132 Referenz zur Bestellposition
+                                                                    null, // Bestelldatum		// BT-132 Referenz zur Bestellposition
+                                                                    null, // Start of billing period
+                                                                    null); // End of billing period
+
+                tradeLineItem.AddApplicableProductCharacteristic("Package size", "100 St");
+                tradeLineItem.AddApplicableProductCharacteristic("Dosage form", "Retard tablets");
+            }
+
+            // add discount
+            // NOTE: If you have items with different VAT rates in your invoice and you want to give a dscount
+            //       on the whole invoice, you might want to split the whole discount into different parts for
+            //       the different VAT rates.
+            desc.AddTradeAllowance(null,
+                                   CurrencyCodes.EUR,
+                                   1.99m,
+                                   null,
+                                   "Auftragsrabatt",
+                                   TaxTypes.VAT,
+                                   TaxCategoryCodes.S,
+                                   19,
+                                   null);
+
+            // add VAT information
+            Decimal taxAmount7 = Math.Round(207.36m / 100m * 7m, 2, MidpointRounding.AwayFromZero);
+            desc.AddApplicableTradeTax(basisAmount: 207.36m,
+                                       percent: 7m,
+                                       taxAmount: taxAmount7,
+                                       typeCode: TaxTypes.VAT,
+                                       categoryCode: TaxCategoryCodes.S
+                                       );
+
+            Decimal taxAmount19 = Math.Round((42.13m + 25.33m  - 1.99m) / 100m * 19m, 2, MidpointRounding.AwayFromZero);
+            desc.AddApplicableTradeTax(basisAmount: 67.46m - 1.99m,
+                                       percent: 19m,
+                                       taxAmount: taxAmount19,
+                                       typeCode: TaxTypes.VAT,
+                                       categoryCode: TaxCategoryCodes.S
+                                       );
+
+            // add totals
+            Decimal lineTotal = 207.36m + 67.46m;
+            Decimal allowanceTotal = 1.99m;
+            Decimal taxTotal = taxAmount7 + taxAmount19;
+            desc.SetTotals(lineTotalAmount: lineTotal,
+                           allowanceTotalAmount: allowanceTotal,
+                           taxBasisAmount: lineTotal - allowanceTotal,
+                           taxTotalAmount: taxTotal,
+                           grandTotalAmount: lineTotal - allowanceTotal + taxTotal,
+                           duePayableAmount: lineTotal - allowanceTotal + taxTotal
+                          );
+
+            desc.AddTradePaymentTerms("Zahlbar innerhalb 30 Tagen netto bis 04.04.2018, 3% Skonto innerhalb 10 Tagen bis 15.03.2018");
+            desc.SetPaymentMeans(PaymentMeansTypeCodes.SEPACreditTransfer); // BG-17 Überweisung
+
+            // Bei Überweisung (BG-17) müssen wir die Kontodaten des Verkäufers angeben.
+            desc.AddCreditorFinancialAccount(iban: "DE43123456789012345678", // BT-84 - IBAN des Zahlungsempfängers
+                                             bic: "", // BT-86 - BIC des Zahlungsempfängers (bei deutschen Banken nicht zwingend erforderlich)
+                                             name: "Lieferant GmbH");
+
+            return desc;
+        }
+    }
+}
